caf116534aaabad1341cb61d6bb362ff2f5145c3
qlDanil <zaharenkodanil1@gmail.com>
2020-11-23 23:33:47 +0700

    OCR

--- mainApp/ocr.py
+++ mainApp/ocr.py
@@ -2,20 +2,24 @@ from PIL import Image
 import pytesseract
 import cv2
 import os
-
+from urllib.request import urlopen
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
 
-def getText(image):
+def getText(url):
     # загрузить образ и преобразовать его в оттенки серого
-    image = cv2.imread(str(image))
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (7, 7), 0)
-    result = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    # сохраним временную картинку в оттенках серого, чтобы можно было применить к ней OCR
-    filename = "{}.png".format(os.getpid())
-    cv2.imwrite(filename, result)
-    # загрузка изображения в виде объекта image Pillow, применение OCR, а затем удаление временного файла
-    text = pytesseract.image_to_string(Image.open(filename))
-    os.remove(filename)
-    return text
+    try:
+        image = Image.open(urlopen(url))
+        image = cv2.imread(str(image))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (7, 7), 0)
+        result = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        # сохраним временную картинку в оттенках серого, чтобы можно было применить к ней OCR
+        filename = "{}.png".format(os.getpid())
+        cv2.imwrite(filename, result)
+        # загрузка изображения в виде объекта image Pillow, применение OCR, а затем удаление временного файла
+        text = pytesseract.image_to_string(Image.open(filename))
+        os.remove(filename)
+        return text
+    except:
+        return "OCR error"
