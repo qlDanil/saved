be290fbea5d2ddd1c44e4fed230579fba22f6c40
qlDanil <zaharenkodanil1@gmail.com>
2020-11-24 21:11:12 +0700

    OCR

--- mainApp/ocr.py
+++ mainApp/ocr.py
@@ -3,14 +3,17 @@ import pytesseract
 import cv2
 import os
 from urllib.request import urlopen
+import numpy as np
+
 pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 
 
 def getText(url):
     # загрузить образ и преобразовать его в оттенки серого
     try:
-        image = Image.open(urlopen(url))
-        image = cv2.imread(str(image))
+        req = urlopen(url)
+        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+        image = cv2.imdecode(arr, -1)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (7, 7), 0)
         result = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
@@ -22,4 +25,4 @@ def getText(url):
         os.remove(filename)
         return text
     except:
-        return "OCR error"
+        return "OCR Error"
